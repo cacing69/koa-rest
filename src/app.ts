758d958ef53635a1e6fa45e6bd2956752f9e061f
Ibnul Mutaki <36250619+cacing69@users.noreply.github.com>
2025-05-03 08:26:11 +0700

    refactor: cleansing app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,38 +1,34 @@
-// import koaWinston from 'koa-winston';
-import Koa from "koa";
-import bodyParser from "koa-bodyparser";
 import { errorHandlerMiddleware } from "./shared/middlewares/error-handler.middleware";
-import swaggerMiddleware from "./shared/middlewares/swagger.middleware";
+import bodyParser from "koa-bodyparser";
+import Koa from "koa";
 import koaCors from 'koa2-cors';
+import koaHelmet from 'koa-helmet';
+import path from 'path';
 import serve from 'koa-static';
+import swaggerMiddleware from "./shared/middlewares/swagger.middleware";
 
 import authRouter from "./features/auth/auth.router";
 import testRouter from './features/test/test.router';
 import userRouter from "./features/user/user.router";
-import path from 'path';
-import koaHelmet from 'koa-helmet';
 
 const app = new Koa();
 
 app.use(errorHandlerMiddleware);
 app.use(bodyParser());
-// Mengonfigurasi middleware CORS
+
+
 app.use(koaCors({
-    origin: '*', // Izinkan semua origin (bisa disesuaikan sesuai kebutuhan)
-    allowMethods: ['HEAD', 'GET', 'PATCH', 'POST', 'PUT', 'DELETE'], // Metode yang diizinkan
-    allowHeaders: ['Content-Type', 'Authorization'], // Header yang diizinkan
+    origin: '*',
+    allowMethods: ['HEAD', 'GET', 'PATCH', 'POST', 'PUT', 'DELETE'],
+    allowHeaders: ['Content-Type', 'Authorization'],
 }));
 app.use(swaggerMiddleware);
 
 app.use(serve(path.join(__dirname, 'public')));
 app.use(serve(path.join(__dirname, 'uploadsyarn')));
 
-// Gunakan koa-helmet untuk menambahkan header keamanan
-app.use(koaHelmet());
 
-// app.use(koaWinston.logger({
-//     winstonInstance: logger,
-// }));
+app.use(koaHelmet());
 
 // Menggunakan koa2-winston untuk logging
 // app.use(koa2Winston({
