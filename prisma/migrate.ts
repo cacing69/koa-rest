e10c69d05834327d4163da60a5428a4a2bf928f3
Ibnul Mutaki <36250619+cacing69@users.noreply.github.com>
2025-05-07 00:29:28 +0700

    fix: update command migrate

--- prisma/migrate.ts
+++ prisma/migrate.ts
@@ -1,8 +1,15 @@
 import { execSync } from 'child_process';
 
 const timestamp = new Date().toISOString().replace(/[-T:.Z]/g, '');
-const name = `migrate_${timestamp}`;
+const name = `${timestamp}`;
 
 console.log(`Running migration: ${name}`);
+
+/**
+ * prisma migrate dev
+ *
+ * saat command di atas dijalankan, terminal akan meminta nama untuk migrate yang akan kita buat
+ *
+ */
 
 execSync(`npx prisma migrate dev --name ${name}`, { stdio: 'inherit' });
